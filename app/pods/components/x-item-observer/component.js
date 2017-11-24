022a6067daeb86dcd3057d93dcf887e4fffdaa1d
Andrey Mikhaylov (lolmaus) <lolmaus@gmail.com>
2017-11-24 14:21:16 +0300

    Added observer demo by @lifeart

--- app/pods/components/x-item-observer/component.js
+++ app/pods/components/x-item-observer/component.js
@@ -3,8 +3,53 @@ import layout from '../x-item/template';
 import { computed, observer } from '@ember/object'
 
 
+// Credit: Alex Kanunnikov @lifeart https://github.com/lifeart
 
 export default XItemComponent.extend({
   layout,
 
+  didChangeAttrsConfig: {
+    attrs: [
+      'one',
+      'two',
+      'three',
+      'four',
+      'five',
+    ],
+  },
+
+  didChangeAttrs() {
+      this.set('changedAttrs', Object.keys(this.get('lastChangedAttrs')))
+  },
+
+  lastChangedAttrs: null,
+
+  init() {
+      this.resetLastChangedAttrs();
+      this.initObservers();
+      this._super(...arguments);
+  },
+  createObserverFor(propertyName) {
+      return function (target, key) {
+          this.addChangedAttribute(propertyName);
+      }.bind(this);
+  },
+  initObservers() {
+      (this.get('didChangeAttrsConfig.attrs') || []).forEach((attrName) => {
+          let observerName = `${attrName}@observer`;
+          this[observerName] = this.createObserverFor(attrName);
+          this.addObserver(attrName, this, observerName);
+      });
+  },
+  didUpdateAttrs() {
+      this.didChangeAttrs(this.lastChangedAttrs);
+      this.resetLastChangedAttrs();
+      this._super(...arguments);
+  },
+  addChangedAttribute(name) {
+      this.lastChangedAttrs[name] = this.get(name);
+  },
+  resetLastChangedAttrs() {
+      this.set('lastChangedAttrs', {});
+  }
 });
